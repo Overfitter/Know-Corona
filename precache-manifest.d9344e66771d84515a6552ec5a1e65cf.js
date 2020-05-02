6688c1947b5f21ca59b76e02b469bb42bd617ce0
Jitendra Jangid <jitendra.jangid@zs.com>
2020-05-03 01:57:15 +0530

    Updates

--- precache-manifest.d9344e66771d84515a6552ec5a1e65cf.js
+++ precache-manifest.d9344e66771d84515a6552ec5a1e65cf.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3e86d9ac0c139afa5216569563755dfc",
+    "revision": "d4f7a36b528b919aaa5ee42b4c47a273",
     "url": "/GoCoronaGo/index.html"
   },
   {
-    "revision": "2661337843df8e7df497",
+    "revision": "ffd10a156d282f856b8b",
     "url": "/GoCoronaGo/static/css/main.70abab53.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/GoCoronaGo/static/js/2.4fc2e24d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2661337843df8e7df497",
-    "url": "/GoCoronaGo/static/js/main.5f9da871.chunk.js"
+    "revision": "ffd10a156d282f856b8b",
+    "url": "/GoCoronaGo/static/js/main.86321156.chunk.js"
   },
   {
     "revision": "f0c6ddfae4ce969ffb24",
