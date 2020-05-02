63911300de99839419d8866d06e545583c5f99c9
Jitendra Jangid <jitendra.jangid@zs.com>
2020-05-02 23:26:09 +0530

    Updates

---
+++ precache-manifest.5c42b26587ec64f9144c53b9f5f18620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bde41d25c546bb809b2d34de4bb4f6",
+    "url": "/GoCoronaGo/index.html"
+  },
+  {
+    "revision": "811d28703b38a61c9798",
+    "url": "/GoCoronaGo/static/css/main.84caf06d.chunk.css"
+  },
+  {
+    "revision": "e946f4e95c7e60689123",
+    "url": "/GoCoronaGo/static/js/2.4fc2e24d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GoCoronaGo/static/js/2.4fc2e24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811d28703b38a61c9798",
+    "url": "/GoCoronaGo/static/js/main.c8b2dce6.chunk.js"
+  },
+  {
+    "revision": "f0c6ddfae4ce969ffb24",
+    "url": "/GoCoronaGo/static/js/runtime-main.88daffd9.js"
+  }
+]);
